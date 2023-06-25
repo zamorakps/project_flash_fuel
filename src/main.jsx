b9d3a7ce5f468dae30a349c3954968113495f078
ParadoxicalHippie <paradoxicalhippie@BigBrother2.0>
2023-06-25 17:51:14 -0500

    Added router to get to new pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ProfileManagementPage from './routes/ProfileManagementPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/ProfileManagement",
+    element: <ProfileManagementPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
