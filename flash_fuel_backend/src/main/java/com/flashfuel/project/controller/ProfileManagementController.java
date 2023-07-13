26629c650fad549bf815a5077535c10a484d75d1
ParadoxicalHippie <paradoxicalhippie@BigBrother2.0>
2023-07-13 18:23:18 -0500

    Connect Profile Management page to backend

--- flash_fuel_backend/src/main/java/com/flashfuel/project/controller/ProfileManagementController.java
+++ flash_fuel_backend/src/main/java/com/flashfuel/project/controller/ProfileManagementController.java
@@ -20,73 +20,16 @@ public class ProfileManagementController {
         _service = service;
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/profile/update")
     public ResponseEntity<?> updateProfile(@ModelAttribute UserProfileRequest profileRequest) {
         return ResponseEntity.ok(_service.updateProfile(profileRequest));
-        /*String username = profileRequest.getUsername();
-
-        if (!userManager.isUserRegistered(username)) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User not found.");
-        }
-
-        if (profileRequest.getName() == null || profileRequest.getName().isEmpty()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Name is required.");
-        }
-
-        if (profileRequest.getAddress() == null || profileRequest.getAddress().isEmpty()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Address is required.");
-        }
-
-        if (profileRequest.getCity() == null || profileRequest.getCity().isEmpty()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("City is required.");
-        }
-
-        if (profileRequest.getState() == null || profileRequest.getState().isEmpty()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("State is required.");
-        }
-
-        if (profileRequest.getZipCode() == null || profileRequest.getZipCode().isEmpty()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Zip Code is required.");
-        } else {
-            String zipCodePattern = "^\\d{5}(?:[-\\s]\\d{4})?$";
-            if (!profileRequest.getZipCode().matches(zipCodePattern)) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid Zip Code format.");
-            }
-        }
-
-        userManager.updateUserCredentials(
-                username,
-                profileRequest.getName(),
-                profileRequest.getAddress(),
-                profileRequest.getAddressLine2(),
-                profileRequest.getCity(),
-                profileRequest.getState(),
-                profileRequest.getZipCode()
-        );
-
-        return ResponseEntity.ok("Profile updated successfully.");*/
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/profile")
     public ResponseEntity<UserProfileResponse> getProfile(@RequestParam String username) {
-        /*if (!userManager.isUserRegistered(username)) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }*/
-
         return ResponseEntity.ok(_service.getProfile(username));
-
-/*        User user = userManager.getUserByUsername(username);
-
-        UserProfileResponse profileResponse = new UserProfileResponse();
-        profileResponse.setUsername(user.getUsername());
-        profileResponse.setName(user.getName());
-        profileResponse.setAddress(user.getAddress());
-        profileResponse.setAddressLine2(user.getAddressLine2());
-        profileResponse.setCity(user.getCity());
-        profileResponse.setState(user.getState());
-        profileResponse.setZipCode(user.getZipCode());
-
-        return ResponseEntity.ok(profileResponse);*/
     }
 
 
