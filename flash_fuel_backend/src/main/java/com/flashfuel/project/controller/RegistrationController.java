b2783a3a378cfacc1ab56e0cb31f437a74d10198
zamorakps <rafey.alvi@gmail.com>
2023-07-15 00:44:35 +0200

    Login and Registration Service Layer + Tests

--- flash_fuel_backend/src/main/java/com/flashfuel/project/controller/RegistrationController.java
+++ flash_fuel_backend/src/main/java/com/flashfuel/project/controller/RegistrationController.java
@@ -1,60 +1,21 @@
 package com.flashfuel.project.controller;
 
-import com.flashfuel.project.UserManager;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import com.flashfuel.project.service.RegistrationService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class RegistrationController {
 
-    private final UserManager userManager;
+    private final RegistrationService registrationService;
 
-    @Autowired
-    public RegistrationController(UserManager userManager) {
-        this.userManager = userManager;
+    public RegistrationController(RegistrationService registrationService) {
+        this.registrationService = registrationService;
     }
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody RegistrationRequest registrationRequest) {
-        String username = registrationRequest.getUsername();
-        String password = registrationRequest.getPassword();
-
-        // Check if the username is already taken
-        if (userManager.isUserRegistered(username)) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username is already taken");
-        }
-
-        // Add the user to the registered users
-        userManager.registerUser(username, password);
-
-        // Return a success response
-        return ResponseEntity.ok("Registration successful");
-    }
-
-    private static class RegistrationRequest {
-        private String username;
-        private String password;
-
-        public String getUsername() {
-            return username;
-        }
-
-        public void setUsername(String username) {
-            this.username = username;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
+    public ResponseEntity<?> login(@RequestParam String username, @RequestParam String password) {
+        return ResponseEntity.ok(registrationService.registerUser(username, password));
     }
 }
---
+++ flash_fuel_backend/src/test/java/RegistrationServiceTest.java
@@ -0,0 +1,57 @@
+import com.flashfuel.project.UserManager;
+import com.flashfuel.project.service.RegistrationService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class RegistrationServiceTest {
+
+    private RegistrationService registrationService;
+    private UserManager userManager;
+
+    @BeforeEach
+    public void setup() {
+        userManager = new UserManager();
+        registrationService = new RegistrationService(userManager);
+    }
+
+    @Test
+    public void testRegisterUser_NewUsername_ReturnsTrue() {
+        String username = "testUser";
+        String password = "testPassword";
+
+        boolean result = registrationService.registerUser(username, password);
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void testRegisterUser_ExistingUsername_ReturnsFalse() {
+        String username = "existingUser";
+        String password = "testPassword";
+        userManager.registerUser(username, password);
+
+        boolean result = registrationService.registerUser(username, password);
+
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void testRegisterUser_EmptyUsername_ReturnsFalse() {
+        String username = "";
+        String password = "testPassword";
+
+        boolean result = registrationService.registerUser(username, password);
+
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void testRegisterUser_NullPassword_ReturnsFalse() {
+        String username = "testUser";
+
+        boolean result = registrationService.registerUser(username, null);
+
+        Assertions.assertFalse(result);
+    }
+}
