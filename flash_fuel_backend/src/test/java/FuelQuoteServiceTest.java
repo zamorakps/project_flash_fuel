42dbbf5401d0bceb887e5689786720be6c571870
lllacoxlll <lllacoxlll@gmail.com>
2023-07-14 23:21:05 -0500

    Added unit tests for fuel quote service

--- flash_fuel_backend/src/test/java/FuelQuoteServiceTest.java
+++ flash_fuel_backend/src/test/java/FuelQuoteServiceTest.java
@@ -1,11 +1,20 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.MockitoAnnotations;
+
 import com.flashfuel.project.FuelQuoteManager;
 import com.flashfuel.project.UserManager;
-import com.flashfuel.project.model.AddFuelQuoteResponse;
 import com.flashfuel.project.model.FuelQuote;
 import com.flashfuel.project.model.FuelQuoteRequest;
 import com.flashfuel.project.model.FuelQuoteResponse;
@@ -13,14 +22,12 @@ import com.flashfuel.project.model.User;
 import com.flashfuel.project.service.FuelQuoteService;
 import com.flashfuel.project.service.ProfileManagementService;
 
-import java.time.LocalDate;
-import java.util.List;
+// Add imports for your classes
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
-@ExtendWith(MockitoExtension.class)
 public class FuelQuoteServiceTest {
+
+    @InjectMocks
+    private FuelQuoteService fuelQuoteService;
 
     @Mock
     private FuelQuoteManager fuelQuoteManager;
@@ -31,69 +38,59 @@ public class FuelQuoteServiceTest {
     @Mock
     private ProfileManagementService profileManagementService;
 
-    @InjectMocks
-    private FuelQuoteService fuelQuoteService;
-
-    @Test
-    void testAddFuelQuote() {
-        FuelQuoteRequest request = new FuelQuoteRequest();
-        request.setUsername("testUser");
-        request.setGallonsRequested(10);
-        request.setDeliveryAddress("123 Fake St");
-        request.setDeliveryDate(LocalDate.now().plusDays(1));
-        request.setSuggestedPrice(2.0);
-        request.setTotalAmountDue(20.0);
-
-        when(userManager.getUserByUsername("testUser")).thenReturn(new User());
-
-        AddFuelQuoteResponse response = fuelQuoteService.addFuelQuote(request);
-
-        assertEquals("Fuel quote added successfully.", response.getMessage());
-        assertEquals(request.getSuggestedPrice(), response.getSuggestedPrice());
-        assertEquals(request.getTotalAmountDue(), response.getTotalAmountDue());
-
-        verify(fuelQuoteManager, times(1)).addFuelQuote(any(User.class), anyInt(), anyString(), any(LocalDate.class), anyDouble(), anyDouble());
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
-    void testAddFuelQuoteInvalidRequest() {
-        FuelQuoteRequest request = new FuelQuoteRequest();
-        request.setUsername("testUser");
-        request.setGallonsRequested(0);
-        request.setDeliveryAddress("123 Fake St");
-        request.setDeliveryDate(LocalDate.now().plusDays(1));
-        request.setSuggestedPrice(2.0);
-        request.setTotalAmountDue(20.0);
-
-        AddFuelQuoteResponse response = fuelQuoteService.addFuelQuote(request);
-
-        assertTrue(response.getMessage().contains("Gallons requested must be greater than 0."));
-    }
-
-    @Test
-    void testGetFuelQuoteHistory() {
-        when(fuelQuoteManager.getFuelQuotesByUsername("testUser")).thenReturn(List.of(new FuelQuote()));
-
-        List<FuelQuoteResponse> fuelQuoteResponses = fuelQuoteService.getFuelQuoteHistory("testUser");
-
-        assertFalse(fuelQuoteResponses.isEmpty());
-        verify(fuelQuoteManager, times(1)).getFuelQuotesByUsername("testUser");
-    }
-
-    @Test
-    void testTransformFuelQuoteToResponse() {
+    public void testGetFuelQuoteHistory() {
+        // Set up test data
         User user = new User();
         user.setUsername("testUser");
+        FuelQuote fuelQuote = new FuelQuote(user, 500, "123 Fake St", LocalDate.now(), 2.5, 1250.0);
+        List<FuelQuote> fuelQuotes = new ArrayList<>(Arrays.asList(fuelQuote));
 
-        FuelQuote fuelQuote = new FuelQuote(user, 10, "123 Fake St", LocalDate.now(), 2.0, 20.0);
+        // Configure mocks
+        when(fuelQuoteManager.getFuelQuotesByUsername("testUser")).thenReturn(fuelQuotes);
 
-        FuelQuoteResponse response = fuelQuoteService.transformFuelQuoteToResponse(fuelQuote);
+        // Call the method under test
+        List<FuelQuoteResponse> result = fuelQuoteService.getFuelQuoteHistory("testUser");
 
-        assertEquals(fuelQuote.getUser().getUsername(), response.getUsername());
-        assertEquals(fuelQuote.getGallonsRequested(), response.getGallonsRequested());
-        assertEquals(fuelQuote.getDeliveryAddress(), response.getDeliveryAddress());
-        assertEquals(fuelQuote.getDeliveryDate(), response.getDeliveryDate());
-        assertEquals(fuelQuote.getSuggestedPrice(), response.getSuggestedPrice());
-        assertEquals(fuelQuote.getTotalAmountDue(), response.getTotalAmountDue());
+        // Verify interactions
+        verify(fuelQuoteManager).getFuelQuotesByUsername("testUser");
+
+        // Assert results
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals("testUser", result.get(0).getUsername());
+    }
+
+    @Test
+    public void testAddFuelQuote() {
+        // Set up test data
+        User user = new User();
+        user.setUsername("testUser");
+        FuelQuoteRequest request = new FuelQuoteRequest();
+        request.setUsername("testUser");
+        request.setGallonsRequested(500);
+        request.setDeliveryAddress("123 Fake St");
+        request.setDeliveryDate(LocalDate.now());
+        request.setSuggestedPrice(2.5);
+        request.setTotalAmountDue(1250.0);
+
+        // Configure mocks
+        when(userManager.getUserByUsername("testUser")).thenReturn(user);
+
+        // Call the method under test
+        com.flashfuel.project.model.AddFuelQuoteResponse result = fuelQuoteService.addFuelQuote(request);
+
+        // Verify interactions
+        verify(userManager).getUserByUsername("testUser");
+        verify(fuelQuoteManager).addFuelQuote(any(User.class), eq(500), eq("123 Fake St"), any(LocalDate.class), eq(2.5), eq(1250.0));
+
+        // Assert results
+        assertNotNull(result);
+        assertEquals("Fuel quote added successfully.", result.getMessage());
     }
 }
