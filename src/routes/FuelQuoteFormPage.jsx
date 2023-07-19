7437df2032b1b45999541ebd909c6a5c8851914a
lllacoxlll <lllacoxlll@gmail.com>
2023-07-19 15:36:59 -0500

    Updated FuelQuoteForm and FuelQuoteHistory pages to display entire address from backend

--- src/routes/FuelQuoteFormPage.jsx
+++ src/routes/FuelQuoteFormPage.jsx
@@ -10,6 +10,132 @@ const FuelQuoteForm = () => {
   const [totalAmountDue, setTotalAmountDue] = useState("");
   const navigate = useNavigate();
 
+  const loggedUserId = 1;
+
+  useEffect(() => {
+    fetch(`http://localhost:8080/api/user/profile?userId=${loggedUserId}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        // const clientInfo = data.clientInformation;
+        const clientInfo = data;
+        const fullAddress = `${clientInfo.address}, ${clientInfo.addressLine2}, ${clientInfo.city}, ${clientInfo.state}, ${clientInfo.zipCode}`;
+        const fullUserProfile = {
+          ...data,
+          fullAddress,
+        };
+        setUserProfile(fullUserProfile);
+        console.log('User profile:', fullUserProfile);
+      });
+  }, []);
+
+  useEffect(() => {
+    if(gallonsRequested !== "") {
+      fetch("http://localhost:8080/api/fuelquote/calculate", {
+        method: "POST",
+        body: JSON.stringify({"gallonsRequested": gallonsRequested}),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Calculated prices:', data);
+        setSuggestedPrice(data.suggestedPrice);
+        setTotalAmountDue(data.totalAmountDue);
+      })
+      .catch(error => {
+        console.log('There was a problem with the calculation request:', error);
+      });
+    }
+  }, [gallonsRequested]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('handleSubmit triggered');
+
+    const fuelQuoteRequest = {
+      "userId": loggedUserId,
+      "gallonsRequested": Number(gallonsRequested),
+      "deliveryAddress": userProfile.fullAddress,
+      "deliveryDate": deliveryDate,
+      "suggestedPrice": suggestedPrice,
+      "totalAmountDue": totalAmountDue,
+    };
+
+    console.log(fuelQuoteRequest);
+
+    fetch("http://localhost:8080/api/fuelquote/new", {
+      method: "POST",
+      body: JSON.stringify(fuelQuoteRequest),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Fuel quote added:', data);
+      navigate('/FuelQuoteHistory');
+    })
+    .catch(error => {
+      console.log('There was a problem with the request:', error);
+    });
+  };
+
+  return (
+    <div className="FormContainer w-1/3">
+      <h1 className="FormTitle">Fuel Quote Form</h1>
+      <form className="FormRoot" onSubmit={handleSubmit}>
+        <div className="FormField">
+          <label className="FormLabel">Gallons Requested</label>
+          <input className="Input" type="number" value={gallonsRequested} onChange={(e) => setGallonsRequested(e.target.value)} required />
+        </div>
+        <div className="FormField">
+          <label className="FormLabel">Delivery Address</label>
+          <input className="Input" value={userProfile.fullAddress || ''} readOnly />
+        </div>
+        <div className="FormField">
+          <label className="FormLabel">Delivery Date</label>
+          <input className="Input" type="date" value={deliveryDate} onChange={(e) => setDeliveryDate(e.target.value)} required />
+        </div>
+        <div className="FormField">
+          <label className="FormLabel">Suggested Price per Gallon</label>
+          <input className="Input" value={suggestedPrice || ''} readOnly />
+        </div>
+        <div className="FormField">
+          <label className="FormLabel">Total Amount Due</label>
+          <input className="Input" value={totalAmountDue || ''} readOnly />
+        </div>
+        <button type="submit" className="border-solid shadow w-full text-center duration-300 -transparent hover:bg-white hover:text-black text-white font-semibold py-2 px-4 border border-lightgray hover:border-transparent rounded" style={{ marginTop: 10 }}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default FuelQuoteForm;
+
+
+
+/*
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../styles/FuelQuoteFormPageStyles.css'
+
+const FuelQuoteForm = () => {
+  const [userProfile, setUserProfile] = useState({});
+  const [gallonsRequested, setGallonsRequested] = useState("");
+  const [deliveryDate, setDeliveryDate] = useState("");
+  const [suggestedPrice, setSuggestedPrice] = useState("");
+  const [totalAmountDue, setTotalAmountDue] = useState("");
+  const navigate = useNavigate();
+
   const loggedUserId = 'testUser';
 
   useEffect(() => {
@@ -113,3 +239,4 @@ const FuelQuoteForm = () => {
 };
 
 export default FuelQuoteForm;
+*/
